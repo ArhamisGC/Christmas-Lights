a8113acf656f780a5e287a9940d85779207f3b8e
hunte <hunterofseries13@gmail.com>
2024-08-20 00:59:36 +0100

    Sranger things wall done

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const lights = document.getElementById("lights");
 
 document.addEventListener('keydown', function(event) {
   let selected = lights.children[event.keyCode - 65];
-  selected.style.animation = "lighting-1 1.25s infinite linear";
+  position(event.keyCode-65,selected);
+  console.log(event.keyCode-65);
 });
 
-
+function position(number,selected){
+  if((number + 1)%4 === 2){
+    selected.style.animation = "lighting-3 1.25s 1 linear";
+  }else if ((number)%2 === 0){
+    console.log(number);
+    selected.style.animation = "lighting-2 1.5s linear 1";
+  }else{
+    selected.style.animation = "lighting-1 1.25s 1 linear";
+  }
+  selected.addEventListener("animationend", ()=>{
+    selected.style.animation = "none";
+  })
+}
